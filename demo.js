36e9640570b877bc668e9bac24d226efeab294d5
FORMAL-TECH\Jiaojiao.SUN <jiaojiao.sun@formal-tech.com>
2023-05-19 11:38:38 +0800

    feat: add something new

---
+++ demo.js
@@ -0,0 +1,18 @@
+/**
+ * Calculates the sum of two numbers.
+ * @param {number} a - The first number.
+ * @param {number} b - The second number.
+ * @returns {number} - The sum of the two numbers.
+ */
+function add(a, b) {
+  return a + b;
+}
+
+/**
+ * Greet a person.
+ * @param {string} name - The name of the person.
+ * @returns {string} - A greeting message.
+ */
+function greet(name) {
+  return `Hello, ${name}!`;
+}
